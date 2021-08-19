bdfda204cf6548b060d1c81d1bef4f72cfea9685
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-08-18 22:05:03 -0700

    Initial Commit

---
+++ SwiftUI_Stock_Grabber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Stock_Grabber
+//
+//  Created by Alphonso Sensley II on 8/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Stock_Grabber/SwiftUI_Stock_GrabberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Stock_GrabberApp.swift
+//  SwiftUI_Stock_Grabber
+//
+//  Created by Alphonso Sensley II on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Stock_GrabberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
