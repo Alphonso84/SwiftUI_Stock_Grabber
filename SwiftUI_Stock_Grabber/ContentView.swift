9a55d7ad367d003f4144595f8cc7380159c1ae2e
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2021-08-20 08:11:16 -0700

    Added timeStamp & favorites feature

--- SwiftUI_Stock_Grabber/ContentView.swift
+++ SwiftUI_Stock_Grabber/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView: View {
     @State private var stockObject:Stock?
     @State private var isFetchingData = false
     @State private var errorOccured = false
+    @State private var timeStamp = ""
+    @State private var isFavorite = false
+    @State private var favorites:[Stock] = []
     
     var body: some View {
         NavigationView {
@@ -29,13 +32,25 @@ struct ContentView: View {
                                 .font(.largeTitle)
                                 .padding(.bottom, 15)
                             VStack(alignment:.leading) {
+                                Text("As Of \(timeStamp)")
                                 Text("Opening Price: \(stockObject.open.asCurrency())")
                                 Text("High Price: \(stockObject.high.asCurrency())")
                                 Text("Low Price: \(stockObject.low.asCurrency())")
+                                Text("Current Price: \(stockObject.current.asCurrency())")
                                 HStack {
                                 Image(systemName:stockObject.current.asCurrency() > stockObject.open.asCurrency() ? "arrow.up.square.fill" : "arrow.down.app.fill")
-                                Text("Current Price: \(stockObject.current.asCurrency())")
-                                    .background(stockObject.current.asCurrency() > stockObject.open.asCurrency() ? Color.green : Color.red)
+                                    Text(stockObject.current.asCurrency() > stockObject.open.asCurrency() ? "UP \((stockObject.current - stockObject.open).asCurrency())" : "DOWN \((stockObject.open - stockObject.current).asCurrency())")
+                                        .background(stockObject.current.asCurrency() > stockObject.open.asCurrency() ? Color.green : Color.red)
+                                }
+                                HStack {
+                                    Image(systemName: isFavorite ? "star.fill" : "star")
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .onTapGesture {
+                                            isFavorite.toggle()
+                                            favorites.append(stockObject)
+                                        }
+                                        .background(isFavorite ? Color.red : Color.gray)
+                                    Text(isFavorite ? "Favorited!" :"Tap To Favorite")
                                 }
                             }
                         }
@@ -46,16 +61,20 @@ struct ContentView: View {
                     
                     if #available(iOS 15.0, *) {
                         Button("Get Stock") {
+                            isFavorite = false
                             isFetchingData = true
                             errorOccured = false
                             Service.getStockForSymbol(symbol: stockSymbol) { stock, error in
                                 print("Call Finished!!")
                                 if error != nil {
                                     errorOccured = true
+                                    timeStamp = ""
                                 }
                                 self.stockObject = stock
+                                timeStamp = getDate()
                                 isFetchingData = false
                                 self.stockSymbol = ""
+                                print("Favorites Array:.... \(favorites)")
                             }
                         }
                         .foregroundColor(Color.white)
@@ -73,9 +92,16 @@ struct ContentView: View {
                     
                 }.navigationTitle("Stock Picker")
             }
-            
         }
     }
+    
+    func getDate()->String{
+        let time = Date()
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:mm"
+        let stringDate = timeFormatter.string(from: time)
+        return stringDate
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
